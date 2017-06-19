5790958e7a346736c097745f4c374cb05b623826
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-06-19 23:10:31 +0100

    Add clickable colour buttons on homepage and tests

--- app/image_search.rb
+++ app/image_search.rb
@@ -2,4 +2,13 @@ class ImageSearchEngine < Sinatra::Base
   get '/' do
     erb :'index'
   end
+
+  post '/search' do
+    erb :'search'
+  end
+
+  get '/search' do
+    @colour = params[:colour]
+    erb :'search'
+  end
 end
---
+++ spec/feature/click_colour_options_spec.rb
@@ -0,0 +1,64 @@
+require 'spec_helper'
+
+feature 'Click Colour Options' do
+
+  before do
+    visit('/')
+  end
+
+  scenario 'Show colour options' do
+    expect(page).to have_content 'Red'
+    expect(page).to have_content 'Pink'
+    expect(page).to have_content 'Orange'
+    expect(page).to have_content 'Yellow'
+    expect(page).to have_content 'Green'
+    expect(page).to have_content 'Blue'
+    expect(page).to have_content 'Purple'
+  end
+
+  scenario 'Clickable colour options' do
+    expect(page).to have_button 'Red'
+    expect(page).to have_button 'Pink'
+    expect(page).to have_button 'Orange'
+    expect(page).to have_button 'Yellow'
+    expect(page).to have_button 'Green'
+    expect(page).to have_button 'Blue'
+    expect(page).to have_button 'Purple'
+
+  end
+
+  scenario 'Click Red option' do
+    click_button "Red"
+    expect(page).to have_content 'Red'
+  end
+
+  scenario 'Click Pink option' do
+    click_button "Pink"
+    expect(page).to have_content 'Pink'
+  end
+
+  scenario 'Click Orange option' do
+    click_button "Orange"
+    expect(page).to have_content 'Orange'
+  end
+
+  scenario 'Click Yellow option' do
+    click_button "Yellow"
+    expect(page).to have_content 'Yellow'
+  end
+
+  scenario 'Click Green option' do
+    click_button "Green"
+    expect(page).to have_content 'Green'
+  end
+
+  scenario 'Click Blue option' do
+    click_button "Blue"
+    expect(page).to have_content 'Blue'
+  end
+
+  scenario 'Click Purple option' do
+    click_button "Purple"
+    expect(page).to have_content 'Purple'
+  end
+end
